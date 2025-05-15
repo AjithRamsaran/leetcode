7be4865a51896ee6c09b22da00b100d0fe19a7a4
Ajith Ramsaran <mail2ajithramsaran@gmail.com>
2025-05-15 11:25:43 +0530

    191. Number of 1 Bits solved - Easy 7 minutes

--- 191. Number of 1 Bits.py
+++ 191. Number of 1 Bits.py
@@ -1,11 +1,21 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
+        
+        count = 0
+        
+        for i in range(32):
+            if (n >> i) & 1:
+                count += 1
+        return count 
+        
         count = 0
         while (n != 1):
             if n % 2 == 1:
                 count +=1
             n = n // 2
         return count + 1
+    
+
     
 s = Solution()
 print(s.hammingWeight(2147483645)) #output 2147483645

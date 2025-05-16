c9f7b97b4a98bae84c2b36d1c298cd23c5c3dcc1
Ajith Ramsaran <mail2ajithramsaran@gmail.com>
2025-05-17 01:37:57 +0530

    647. Palindromic Substrings solved medium

---
+++ 647. Palindromic Substrings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        def expand_around(l,r):
+            nonlocal count
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+        for i in range(len(s)):
+            expand_around(i, i)
+            expand_around(i, i + 1)
+        return count
+    
+s = Solution()
+print(s.countSubstrings("abc"))

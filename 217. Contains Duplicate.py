5b2484c25ce5033170c527251a4efa87d01d17de
Ajith Ramsaran <mail2ajithramsaran@gmail.com>
2025-05-17 10:30:32 +0530

    217. Contains Duplicate solved Easy

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,16 @@
+#time taken solve:  mins
+from typing import List
+from collections import defaultdict 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for i in nums:
+            if i in s:
+                return True
+            s.add(i)
+        return False
+    
+s = Solution()
+print(s.containsDuplicate([1,2,3,1]))
+print(s.containsDuplicate([1,2,3,4]))
+print(s.containsDuplicate([1,1,1,3,3,4,3,2,4,2]))

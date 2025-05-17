2cf9e763d02d93a96ec8ef49d6c6999588af9f50
Ajith Ramsaran <mail2ajithramsaran@gmail.com>
2025-05-17 10:10:29 +0530

    1. Two Sum solved Easy

---
+++ 1. Two Sum.py
@@ -0,0 +1,13 @@
+#time taken solve:
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pair = {}
+        for i,val in enumerate(nums):
+            if target - val in pair:
+                return [pair[target - val], i]
+            pair[val] = i
+s = Solution()
+print(s.twoSum([2,7,11,15], 9))
+print(s.twoSum([3,2,4], 6))
+print(s.twoSum([3,3],6))

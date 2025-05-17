0d6a4e3761592848da0fc303d7b01c3f7ad5d1f3
Ajith Ramsaran <mail2ajithramsaran@gmail.com>
2025-05-17 11:43:58 +0530

    53. Maximum Subarray solved

---
+++ 53. Maximum Subarray.py
@@ -0,0 +1,17 @@
+#time taken solve:  mins
+from typing import List
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxi = nums[0]
+        prev = nums[0] if nums[0] > 0 else 0
+        for i in nums[1:]:
+            prev = prev + i
+            maxi = max(maxi, prev)
+            if prev < 0:
+                prev = 0
+        return maxi
+s = Solution()
+print(s.maxSubArray(([-2,1,-3,4,-1,2,1,-5,4])))
+print(s.maxSubArray([1]))
+print(s.maxSubArray(([5,4,-1,7,8])))
+print(s.maxSubArray(([-1,-2])))

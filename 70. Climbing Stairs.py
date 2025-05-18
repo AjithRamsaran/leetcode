68514ffe3b26a1db2dba4471b4493c5e71201daa
Ajith Ramsaran <mail2ajithramsaran@gmail.com>
2025-05-18 22:37:26 +0530

    70. Climbing Stairs.py

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 3:
+            return n
+        a = 1
+        b = 2
+        while(n - 2 >0):
+            a, b = b, a + b
+            n -= 1
+        return b
+s = Solution()
+print(s.climbStairs(2))
+print(s.climbStairs(5))
+print(s.climbStairs(45))
+print(s.climbStairs(1))

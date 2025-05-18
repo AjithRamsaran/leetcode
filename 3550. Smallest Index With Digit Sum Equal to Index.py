5ec39db17d8053ad79b64f3502153c08889f1468
Ajith Ramsaran <mail2ajithramsaran@gmail.com>
2025-05-19 00:47:23 +0530

    3550. Smallest Index With Digit Sum Equal to Index solved easy

---
+++ 3550. Smallest Index With Digit Sum Equal to Index.py
@@ -0,0 +1,13 @@
+
+#time taken to solve: 15 mins
+from typing import List
+class Solution:
+    def smallestIndex(self, nums: List[int]) -> int:
+        for i,val in enumerate(nums):
+            s = 0
+            while val != 0:
+                s += val%10
+                val = val // 10
+            if s == i:
+                return i
+        return -1

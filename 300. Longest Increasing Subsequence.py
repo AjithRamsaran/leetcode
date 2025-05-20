ae677b7dbfe357f6a8429019174c3f368c933d78
Ajith Ramsaran <mail2ajithramsaran@gmail.com>
2025-05-20 23:59:43 +0530

    704. Binary Search.py completed

--- 300. Longest Increasing Subsequence.py
+++ 300. Longest Increasing Subsequence.py
@@ -1,15 +1,17 @@
-#time taken to solve: 30 mins
+#time taken to solve: 1 hr
 from typing import List
 from bisect import bisect_left
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # n = len(nums)
+        # n = len(nums) #Dp approach O(n^2)
         # dp = [1] * n
         # for i in range(n):
         #     for j in range(i):
         #         if nums[i] > nums[j] and dp[i] < dp[j] + 1:
         #             dp[i] = dp[j] + 1
         # return max(dp)
+        
+        #Efficient approach with binary search O(n * logn)
         sub = [nums[0]]
         for i in range(1, len(nums)):
             if sub[-1] < nums[i]:
@@ -20,7 +22,7 @@ class Solution:
         return len(sub)
     
 s = Solution()
-print(s.lengthOfLIS([10,9,2,5,3,7,101,18]))
-print(s.lengthOfLIS([0,1,0,3,2,3]))
-print(s.lengthOfLIS([7,7,7,7,7,7,7]))
-print(s.lengthOfLIS([1,3,6,7,9,4,10,5,6]))
+print(s.lengthOfLIS([10,9,2,5,3,7,101,18])) #output 4
+print(s.lengthOfLIS([0,1,0,3,2,3])) #4
+print(s.lengthOfLIS([7,7,7,7,7,7,7])) #1
+print(s.lengthOfLIS([1,3,6,7,9,4,10,5,6])) #6
